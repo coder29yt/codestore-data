8ebf1b675d72e844045097ea62fa428180cc25d5
coder29yt <134164127+coder29yt@users.noreply.github.com>
2025-01-22 17:51:08 +0530

    added handlw donwload invoice

--- OrderData.jsx
+++ OrderData.jsx
@@ -124,69 +124,8 @@ const OrderData = ({
   };
 
   return (
-    <Card className="grid gap-2 p-2">
-      {products.map((product) => (
-        <div
-          key={product._id}
-          className="flex flex-col sm:flex-row justify-between items-end sm:items-center border p-3 rounded-lg bg-gray-100 dark:bg-zinc-900"
-        >
-          <div className="flex items-center gap-2">
-            <img
-              src={product?.id?.images?.[0].url}
-              alt={product?.id?.name}
-              className="w-20 h-20 rounded-lg"
-            />
-            <div className="grid gap-1">
-              <h1 className="font-semibold text-sm sm:text-lg">
-                {product?.id?.name}
-              </h1>
-              <p className="flex text-xs sm:text-md gap-2 sm:gap-2 text-gray-500 my-2 sm:my-0">
-                <span>
-                  Color :{" "}
-                  <span style={{ backgroundColor: product?.color }}>
-                    {product?.color}
-                  </span>
-                </span>
-                <span className="hidden sm:block">|</span>
-                <span>
-                  Status : <span className="capitalize">{status}</span>{" "}
-                </span>
-              </p>
-            </div>
-          </div>
-          <div className="flex sm:flex-col gap-3 sm:gap-0 mt-2 sm:mt-0 sm:items-center">
-            <h2 className="text-md sm:text-xl font-bold flex items-center dark:text-customYellow">
-              <IndianRupee size={18} />
-              {product?.id?.price}
-            </h2>
-            <p className="dark:text-customYellow text-end">
-              Qty: {product?.quantity}
-            </p>
-          </div>
-        </div>
-      ))}
-
-      <div className="flex flex-col sm:flex-row justify-between sm:items-center">
-        <span>
-          Ordered On:{" "}
-          <span className="capiatalize">
-            {new Date(createdAt).toLocaleString()}
-          </span>
-        </span>
-        <span
-          onClick={handleDownloadInvoice}
-          className="hover:underline text-sm cursor-pointer flex items-center gap-1 dark:text-customYellow"
-        >
-          <ArrowDownToLine size={10} />
-          Download Invoice
-        </span>
-      </div>
-
-      <hr />
-      <span>
-        Delivery At: <span className="capitalize">{address}</span>
-      </span>
-    </Card>
+  <>
+  </>
   );
 };
 
