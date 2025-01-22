206ac143c64c60545f54bf501db8f89c28e90572
coder29yt <134164127+coder29yt@users.noreply.github.com>
2025-01-22 17:49:00 +0530

    Update OrderController.js

--- OrderController.js
+++ OrderController.js
@@ -2,96 +2,6 @@ const { ROLES } = require("../utils/constants");
 const Order = require("../models/Order");
 const User = require("../models/User");
 
-const getOrdersByUserId = async (req, res) => {
-  const userId = req.id;
-
-  try {
-    const orders = await Order.find({ userId }).populate({
-      path: "products.id",
-      select: "name price category images",
-    });
-
-    if (!orders)
-      return res
-        .status(500)
-        .json({ success: false, message: "No orders to show" });
-
-    return res.status(200).json({ success: true, data: orders });
-  } catch (error) {
-    return res.status(500).json({ success: false, message: error.message });
-  }
-};
-
-const getAllOrders = async (req, res) => {
-  if (req.role !== ROLES.admin) {
-    return res.status(403).json({
-      success: false,
-      message: "You are not authorized to access this resource",
-    });
-  }
-
-  const { page, limit } = req.query;
-
-  try {
-    const orders = await Order.find()
-      .populate({
-        path: "products.id",
-        select: "name price category images",
-      })
-      .populate({
-        path: "userId",
-        select: "name email",
-      })
-      .limit(limit * 1)
-      .skip((page - 1) * limit)
-      .sort({ createdAt: -1 });
-
-    if (!orders)
-      return res
-        .status(404)
-        .json({ success: false, message: "No orders to show" });
-
-    const count = await Order.countDocuments();
-
-    return res.status(200).json({
-      success: true,
-      data: orders,
-      totalPages: Math.ceil(count / limit),
-      currentPage: Number(page),
-    });
-  } catch (error) {
-    return res.status(500).json({ success: false, message: error.message });
-  }
-};
-
-const updateOrderStatus = async (req, res) => {
-  if (req.role !== ROLES.admin) {
-    return res.status(403).json({
-      success: false,
-      message: "You are not authorized to accesss this resource",
-    });
-  }
-
-  const { paymentId } = req.params;
-  const { status } = req.body;
-
-  try {
-    const order = await Order.findOneAndUpdate(
-      { razorpayPaymentId: paymentId },
-      { status },
-      { new: true }
-    );
-    if (!order)
-      return res
-        .status(404)
-        .json({ success: false, message: "Order not found" });
-    return res
-      .status(200)
-      .json({ success: true, data: order, message: "Order status updated" });
-  } catch (error) {
-    return res.status(500).json({ success: false, message: error.message });
-  }
-};
 
 const getMetrics = async (req, res) => {
   if (req.role !== ROLES.admin) {
@@ -238,8 +148,5 @@ const getMetrics = async (req, res) => {
 };
 
 module.exports = {
-  getOrdersByUserId,
-  getAllOrders,
-  updateOrderStatus,
   getMetrics,
 };
